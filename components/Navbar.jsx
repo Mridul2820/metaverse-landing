48a22c375d53b65b572364c8046f120c9e55a63c
Mridul Panda <immridulpanda2820@gmail.com>
2022-11-26 17:23:02 +0530

    fet started section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 'use client';
 import React from 'react';
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { navVariants } from '../utils/motion';
 import styles from '../styles';
@@ -14,7 +15,9 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img src="/search.svg" alt="search" className="w-8 h-8 object-contain" />
-      <h2 className="font-extrabold text-2xl text-white">METAVERSE</h2>
+      <Link href="/">
+        <h2 className="font-extrabold text-2xl text-white">METAVERSE</h2>
+      </Link>
       <img src="/menu.svg" alt="menu" className="w-8 h-8 object-contain" />
     </div>
   </motion.nav>
--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,3 +1,16 @@
-const StartSteps = () => <div>start steps</div>;
+import styles from '../styles';
+
+const StartSteps = ({ number, text }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-3xl bg-[#323F5D]`}
+    >
+      <p className="font-bold text-[20px] text-white">{number}</p>
+    </div>
+    <p className="flex-1 ml-7 font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {text}
+    </p>
+  </div>
+);
 
 export default StartSteps;
